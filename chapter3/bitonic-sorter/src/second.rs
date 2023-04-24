ec5f81c4dce23e9bea540370aaa477fd93863528
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-24 22:28:07 +0900

    Handle an error.

--- chapter3/bitonic-sorter/src/second.rs
+++ chapter3/bitonic-sorter/src/second.rs
@@ -1,9 +1,18 @@
 use super::SortOrder;
 
-pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
-    match *order {
-        SortOrder::Ascending => do_sort(x, true),
-        SortOrder::Descending => do_sort(x, false),
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+
+        Ok(())
+    } else {
+        Err(format!(
+            "The length of x is not a power of two. (x.len(): {})",
+            x.len()
+        ))
     }
 }
 
@@ -37,6 +46,8 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 // このモジュールは cargo test を実行した時のみコンパイルされる
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::sort;
     use crate::SortOrder::*;
 
@@ -44,7 +55,7 @@ mod tests {
     fn sort_u32_ascending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
 
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
@@ -53,7 +64,7 @@ mod tests {
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
-        sort(&mut x, &Descending);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
@@ -71,7 +82,7 @@ mod tests {
             "GC",
         ];
 
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
 
         assert_eq!(
             x,
@@ -101,7 +112,7 @@ mod tests {
             "GC",
         ];
 
-        sort(&mut x, &Descending);
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
 
         assert_eq!(
             x,
@@ -116,5 +127,11 @@ mod tests {
                 "GC"
             ]
         );
+    }
+
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11];
+        assert!(sort(&mut x, &Ascending).is_err());
     }
 }
