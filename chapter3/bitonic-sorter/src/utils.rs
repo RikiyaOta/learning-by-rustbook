be6557c82472dba3673ba85455e5bc9d7f604c1d
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-25 21:26:51 +0900

    Add sort checker functions.

--- chapter3/bitonic-sorter/src/utils.rs
+++ chapter3/bitonic-sorter/src/utils.rs
@@ -8,3 +8,11 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
 
     rng.sample_iter(&Standard).take(n).collect()
 }
+
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1])
+}
