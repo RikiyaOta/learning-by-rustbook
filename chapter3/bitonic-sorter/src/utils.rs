afe813d2862567ee6da7a0518c8959fb09b77d73
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-25 21:24:06 +0900

    Refactor the random number generator.

--- chapter3/bitonic-sorter/src/utils.rs
+++ chapter3/bitonic-sorter/src/utils.rs
@@ -6,12 +6,5 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
     // RNG を初期化する. シード値固定.
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
 
-    let mut v = Vec::with_capacity(n);
-
-    for _ in 0..n {
-        // Standard = 一様分布
-        v.push(rng.sample(&Standard));
-    }
-
-    v
+    rng.sample_iter(&Standard).take(n).collect()
 }
