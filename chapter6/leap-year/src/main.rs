deb5841ec1f2ff7658c6beeef22a3c40ea369895
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-27 20:42:02 +0900

    Add some modules.

--- chapter6/leap-year/src/main.rs
+++ chapter6/leap-year/src/main.rs
@@ -1,6 +1,7 @@
-use std::io::{self, Read};
+use std::io;
 use std::io::Write;
 
+#[derive(Debug)]
 struct Circle {
     radius: u32
 }
@@ -12,6 +13,18 @@ impl Circle {
     
     fn small_circle() -> Circle {
         Circle { radius: 1 }
+    }
+}
+
+mod server {
+    pub fn echo() {
+        println!("Server");
+    }
+}
+
+mod client {
+    pub fn echo() {
+        println!("Client");
     }
 }
 
@@ -32,9 +45,13 @@ fn main() {
     
     let circle1 = Circle { radius: 10};
     println!("Circle1's diameter: {}", circle1.diameter());
+    println!("Circle1: {:?}", circle1);
     
     let circle2 = Circle::small_circle();
     println!("Circle2's diameter: {}", circle2.diameter());
+    
+    server::echo();
+    client::echo();
 }
 
 fn is_leap_year(year: u32) -> bool {
