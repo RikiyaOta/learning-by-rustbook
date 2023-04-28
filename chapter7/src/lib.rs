f75d7bfffc9c10f3292cbe2dde0d58a6f1bcb52f
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-28 23:43:34 +0900

    Implement `pop` method. I learned std::mem::replace.

--- chapter7/src/lib.rs
+++ chapter7/src/lib.rs
@@ -55,4 +55,15 @@ impl<T: Default> ToyVec<T> {
     }
 
     fn grow(&mut self) {}
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.len -= 1;
+            //let elem = self.elements[self.len];
+            let elem = std::mem::replace(&mut self.elements[self.len], Default::default());
+            Some(elem)
+        }
+    }
 }
