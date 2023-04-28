e6e43a8bb5afd6dacc12f497d9186d3b93f3ab23
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-28 23:47:18 +0900

    Fix example 01.

--- chapter7/examples/toy_vec_01.rs
+++ chapter7/examples/toy_vec_01.rs
@@ -1,16 +1,11 @@
 use chapter7::ToyVec;
 
 fn main() {
-    let e: Option<&String>;
+    let mut v = ToyVec::new();
+    v.push("Java Finch".to_string());
+    v.push("Budgerigar".to_string());
 
-    {
-
-        let mut v = ToyVec::new();
-        v.push("Java Finch".to_string());
-        v.push("Budgerigar".to_string());
-
-        e = v.get(1);
-    }
+    let e = v.get(1);
 
     assert_eq!(e, Some(&"Budgerigar".to_string()));
 }
