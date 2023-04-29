423f3ebf3335678040cccbaa23f97efa89718905
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-04-29 20:33:19 +0900

    Learned closures and ownership.

--- chapter7/examples/ch07_10_closure.rs
+++ chapter7/examples/ch07_10_closure.rs
@@ -41,4 +41,16 @@ fn main() {
     apply_fn_mut(&mut modify, 'e');
     apply_fn_once(modify, 'd');
     assert_eq!(s2, "appended");
+
+    let s3 = "be converted".to_string();
+    // ↓このクロージャーは FnOnce を実装するはず（s3の所有権を奪っているので）
+    let mut consume = |ch| {
+        let bytes = s3.into_bytes(); // into_bytes(self) は String を消費する（所有権を取る）
+        bytes.contains(&(ch as u8))
+    };
+
+    apply_fn_once(consume, 'd');
+
+    // ↓ s3 も move されているので、コンパイルエラー
+    // assert_eq!(s3, "error");
 }
